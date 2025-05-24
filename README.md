c3d4414d0817b9620cd1df632f7d45dfb0388786
deajadrovska <jadrovska.dea@gmail.com>
2025-05-24 11:40:06 +0200

    Added Admin Panel logic

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hot Air Balloon Flight Management System
+
+Django application for managing hot air balloon flights.
+
+## Features
+- Flight management with pilots, balloons, and airlines
+- Pilot-airline relationship management
+- Flight image uploads
+
+## Setup
+1. Clone the repository
+2. Install Django: `pip install django`
+3. Run migrations: `python manage.py migrate`
+4. Create superuser: `python manage.py createsuperuser`
+    There is already a user created:
+    username and password: admin
+5. Run server: `python manage.py runserver`
--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
+from application.models import Flight, Balloon, Airline, Pilot, AirlinePilot
+
+
 # Register your models here.
+
+
+class AirlinePilotInline(admin.TabularInline):
+    model = AirlinePilot
+    extra = 1
+
+
+class AirlineAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    inlines = [AirlinePilotInline,]
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ('flight_number', 'take_off_airport', 'landing_airport')
+    exclude = ('user', )
+
+    def save_model(self, request, obj, form, change):
+        if not change: #ako ne se menuva potocno ako nov se dodava
+            obj.user = request.user
+        # obj.save() - vaka NE
+        return super(FlightAdmin, self).save_model(request, obj, form, change)
+
+    def has_change_permission(self, request, obj=None):
+        return obj and obj.user == request.user
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+class PilotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname')
+
+
+
+
+
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Airline, AirlineAdmin)
+admin.site.register(Balloon)
+admin.site.register(Pilot, PilotAdmin)
+
+
